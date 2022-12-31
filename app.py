597b73730bf6ec2b4a8d182962ae5f5ac3c66665
mariadel1974 <120971528+mariadel1974@users.noreply.github.com>
2022-12-31 20:23:42 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+picFolder=os.path.join('static','pics')
+
+app.config['UPLOAD_FOLDER']=picFolder
+
+@app.route("/")
+def index():
+    pic2 = os.path.join(app.config['UPLOAD_FOLDER'],'pic2.png')
+    return render_template("index.html", user_image = pic2)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 4000)))
