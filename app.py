2e0c34183f0925ec15af18d21ee461d0c79f60dc
mariadel1974 <120971528+mariadel1974@users.noreply.github.com>
2023-01-02 17:46:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def index():
     return render_template("index.html", user_image = pic2)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 4000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
